e4a3b3458c263fce5e394d26a5c5c690a07d6dc9
reinjan <reinjan@feops.com>
2019-07-23 18:26:44 +0200

    vuetify project

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,49 @@
 <template>
-  <div class="Login">
-    <h1> Login </h1>
-    <el-form ref="login-form" @submit.native.prevent="login" :model="form">
-      <el-form-item label="username">
-        <el-input v-model="form.username" type=username></el-input>
-      </el-form-item>
-      <el-form-item label="Password">
-        <el-input v-model="form.password" type=password></el-input>
-      </el-form-item>
-      <el-button class="button" type="primary"
-      :disabled="!formIsComplete" :loading="loading" native-type="submit">
-        {{ loading ? 'Loading...' : 'Login' }}
-      </el-button>
-    </el-form>
-  </div>
+<v-container fluid fill-height>
+    <v-layout justify-center>
+      <v-flex xs12 sm8 md4>
+        <!-- <v-img
+          :src="require('../assets/images/insilc_logo.png')"
+          class="my-3"
+          contain
+          height="60">
+        </v-img> -->
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary" dense flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form @submit.prevent="login" id="login">
+              <v-text-field
+                v-model="form.username"
+                prepend-icon="person"
+                name="username"
+                label="username"
+                autocomplete="username"
+                type="username"></v-text-field>
+              <v-text-field
+                v-model="form.password"
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                id="password"
+                autocomplete="current-password"
+                type="password"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" color="primary" form="login">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+        <p class="ma-2 pa-2 text-md-center">
+          Don't have an account yet? <router-link :to="{name: 'register'}">Create one</router-link>.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
 
 <script>
 import { mapGetters } from 'vuex';
