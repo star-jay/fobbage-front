4e81dcae96372f2ef294cf087c950d20d7b776ba
reinjan <reinjan@feops.com>
2019-07-24 11:35:32 +0200

    install vuetify 2.0

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
-import './plugins/vuetify';
-import router from './router';
-import store from './store';
+import Vuetify from 'vuetify/lib';
 
 import './plugins';
 import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+});
 
 new Vue({
   router,
   store,
+  vuetify: new Vuetify(),
   render: h => h(App),
 }).$mount('#app');
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,19 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
+// import Vue from 'vue';
+// import Vuetify from 'vuetify/lib';
+// import 'vuetify/src/stylus/app.styl';
 
-Vue.use(Vuetify, {
-  iconfont: 'md',
-});
+// Vue.use(Vuetify, {
+//   iconfont: 'md',
+// });
+
+// v2.0
+
+// import Vue from 'vue'
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+
+// Vue.use(Vuetify)
+
+// new Vue({
+//   vuetify: new Vuetify(opts)
+// }).$mount('#app')
