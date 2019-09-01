e010f0753fa74d93a7f42d06aa07a4988edf8ca4
reinjan <reinjan@feops.com>
2019-09-01 14:10:13 +0200

    notify when no action is required

--- src/components/quizes/Guess.vue
+++ src/components/quizes/Guess.vue
@@ -1,27 +1,34 @@
 <template>
   <div>
-    <p v-if="activeQuestion.have_guessed">Your guess was submitted</p>
-    <v-form v-else @submit.prevent="guess" id="guess">
-      <v-list flat>
-        <v-list-item-group v-model="answer" color="primary">
-          <v-list-item
-            v-for="answer in activeQuestion.answers"
-            :key="answer.id"
-          >
-            <template v-slot:default="{ active }">
-              <v-list-item-action>
-                <v-checkbox v-model="active"></v-checkbox>
-              </v-list-item-action>
-              <v-list-item-content>
-              <v-list-item-title v-text="answer.text">
-                </v-list-item-title>
-              </v-list-item-content>
-            </template>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-      <v-btn type="submit" color="primary" form="guess">Guess</v-btn>
-    </v-form>
+    <div v-if="activeQuestion.answers">
+      <p v-if="activeQuestion.have_guessed">Your guess was submitted</p>
+      <v-form v-else @submit.prevent="guess" id="guess">
+        <v-list flat>
+          <v-list-item-group v-model="answer" color="primary">
+            <v-list-item
+              v-for="answer in activeQuestion.answers"
+              :key="answer.id"
+            >
+              <template v-slot:default="{ active }">
+                <v-list-item-action>
+                  <v-checkbox v-model="active"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                <v-list-item-title v-text="answer.text">
+                  </v-list-item-title>
+                </v-list-item-content>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+        <v-btn type="submit" color="primary" form="guess">Guess</v-btn>
+      </v-form>
+    </div>
+    <div v-else>
+      <p>
+        No action required.
+      </p>
+    </div>
   </div>
 </template>
 
